fde7bedd05a6225354bda7fdde9944c042668914
Luke Scherrer <lukescherrer2@gmail.com>
2025-12-17 16:56:07 -0800

    add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Neovim Configuration
+
+This is my personal neovim config. It is lightweight and supports LSP. 
+
+## Installation
+
+To use this config on macOS or Linux, simply clone into `~/.config/nvim`
+
+```sh
+git clone https://github.com/lscherrer2/nvim.git ~/.config/nvim
+```
+
+For Windows, clone into `~/AppData/Local/nvim`:
+
+```sh
+git clone https://github.com/lscherrer2/nvim.git ~/AppData/Local/nvim
+```
+
+## Customization & Plugins
+
+You can use this as a starting point and add your own plugins. Simply add your lazy.nvim plugin 
+spec to the `plugins/` folder.
+
+---
+
+Enjoy!
--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -6,22 +6,11 @@ vim.opt.smartindent = true
 vim.opt.autoindent = false
 vim.opt.smarttab = false
 
--- Optional: make it local per filetype later via ftplugin/
--- Example: 2-space indent for JS/TS
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "javascript", "typescript", "json" },
---   callback = function()
---     vim.opt_local.shiftwidth = 2
---     vim.opt_local.tabstop = 2
---     vim.opt_local.softtabstop = 2
---   end,
--- })
 
--- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit colour
+-- enable 24-bit colour
 vim.opt.termguicolors = true
 
 -- Relative Line Numbers
