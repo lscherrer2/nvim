00f53ece78784991f091300ca53b6aea0c0b0dbc
Luke Scherrer <lukescherrer2@gmail.com>
2025-12-27 19:24:31 -0800

    dap added

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,10 +5,34 @@ return {
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
 			"mason-org/mason.nvim",
+			"jay-babu/mason-nvim-dap.nvim",
+			"mason-org/mason-lspconfig.nvim",
+			"mfussenegger/nvim-dap-python",
 		},
 		config = function()
 			local dap = require("dap")
 			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-python").setup("uv")
+
+			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+
+			vim.keymap.set("n", "<leader>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F11>", dap.restart)
+			vim.keymap.set("n", "<F12>", dap.terminate)
+
+			vim.keymap.set("n", "<leader>duo", ui.open)
+			vim.keymap.set("n", "<leader>duc", ui.close)
 		end,
 	},
 }
